061e564f7ee038a1c126c66dd533c7169cd0d46d
shamam <kafri.shamam@gmail.com>
2024-10-27 09:23:45 +0300

    Initial Commit

---
+++ Hydration/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Hydration
+//
+//  Created by Shamam Alkafri on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
